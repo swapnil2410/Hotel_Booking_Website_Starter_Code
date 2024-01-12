63b56aab049098892bab23214801c6e9c3d95b90
SWAPNIL <swapnilnigam2410@gmail.com>
2024-01-12 18:51:30 +0530

    Checkpoint6

---
+++ scripts/list.js
@@ -0,0 +1,10 @@
+function disableOrEnable() {
+   // alert('yes');
+    document.getElementById("mapview").remove();
+    //return ;
+//      var nodes = document.getElementById("mapview").getElementsByTagName('*');
+// for(var i = 0; i < nodes.length; i++){
+//     alert(nodes[i].nodeName);
+//      nodes[i].disabled = true;
+// }
+}
